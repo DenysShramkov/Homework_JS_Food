b1ebe5739427ac53ab73ea3c4351f8f9935012f7
DenysShramkov <70487050+DenysShramkov@users.noreply.github.com>
2021-08-27 02:39:34 +0300

    Add files via upload
    Add js for forms, POST, GET

--- src/js/script.js
+++ src/js/script.js
@@ -92,7 +92,224 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 		}
 	}
-
 	setClock('.timer', deadLine);
 
+	const btn = document.querySelectorAll('button[data-modal]'),
+		modal = document.querySelector('.modal');
+
+		
+	const openModalTimer = setTimeout(toggleModal, 30000);
+	
+	function closeModal(closeBody) {
+		closeBody.addEventListener('click', (e) => {
+			if (e.target && e.target.classList.contains('modal') || e.target.getAttribute('data-close') == '' ) {
+				toggleModal();
+			}
+		});
+		document.addEventListener('keydown', (e) => {
+			if (e.code === 'Escape' && modal.classList.contains('modal__active')) { //кнопка по коду, для esc - это Escape
+				toggleModal();
+			}
+		});
+	}
+
+	function toggleModal() {
+		modal.classList.toggle('modal__active');
+		clearInterval(openModalTimer);
+		document.body.classList.toggle('overflow-hidden');
+				//чтобы не прокручивался контент за модальным окном
+	}
+
+	function openModal(button) {
+		button.forEach((item) => {
+			item.addEventListener('click', () =>{
+				toggleModal();
+			});
+		});
+	}
+
+ 	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			toggleModal();
+			window.removeEventListener('scroll', showModalByScroll); //ч>тко повторить событие
+			//убрать обработчик можно только через паредачу в него функции
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
+	openModal(btn);
+	closeModal(modal);
+
+	const menuField = document.querySelector('.menu__field > .container');
+
+	class Menu {
+		constructor(imgUrl, imgAlt, menuSubtitle, menuDescr, cost, USDtoUAH, parentSelector, ...classes) {
+			this.imgUrl = imgUrl;
+			this.imgAlt = imgAlt;
+			this.menuSubtitle = menuSubtitle;
+			this.menuDescr = menuDescr;
+			this.cost = cost;
+			this.USDtoUAH = USDtoUAH;
+			this.changeToUAH();
+			this.parentSelector = parentSelector;
+			this.classes = classes;
+		}
+
+		changeToUAH() {
+			this.price = Math.round(this.cost*this.USDtoUAH);
+		}
+		//returnInnerHTML() {
+		render() {
+			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				//проверка не передается ли пустой массив классов, у которого нельзя другим способом указать дефолтное значение
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+				//перебор массивы от ...rest и циклом добавление переданных классов элементу
+			}
+			
+			element.innerHTML = `
+				<img src=${this.imgUrl} alt=${this.imgAlt}>
+				<h3 class="menu__item-subtitle">${this.menuSubtitle}</h3>
+				<div class="menu__item-descr">${this.menuDescr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total">
+					<span>${this.price}</span> грн/день</div>
+				</div>`;
+			this.parentSelector.append(element);
+			//return HTML;
+		}
+	}
+
+	
+	const fitness = new Menu(
+		'img/tabs/vegy.jpg',
+		'vegy',
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		9,
+		28,
+		menuField);
+	const premium = new Menu(
+		'img/tabs/elite.jpg',
+		'elite',
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		20,
+		28,
+		menuField);
+	const postnoe = new Menu(
+		'img/tabs/post.jpg',
+		'post',
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		12,
+		28,
+		menuField,
+		'menu__item',
+		'empty',
+		'more');
+		
+
+	//можно new Menu(args).render(); не создается ссылка
+
+	fitness.render();
+	premium.render();
+	postnoe.render();
+
+	//menuField.innerHTML = fitness.returnInnerHTML() + premium.returnInnerHTML() + postnoe.returnInnerHTML();
+
+	const forms = document.querySelectorAll('form');
+
+	const massage = {
+		loading: 'img/forms/spinner.svg',
+		succes: 'Спасибо, мы с вами свяжемся как можно скорее!',
+		failure: 'Произошла ошибка отправки формы',
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			//submit сработает каждый раз, когда мы попытаемся отправить форму
+			e.preventDefault();
+			const request = new XMLHttpRequest();
+			const statusMassage = document.createElement('img');
+			statusMassage.src = massage.loading;
+			statusMassage.style.cssText = `
+			display: block;
+			margin: 0 auto;
+			`;
+
+			form.insertAdjacentElement('afterend', statusMassage);
+			request.open('POST', 'server.php');
+			//request.setRequestHeader('Content-type', 'multipar/form-data');
+			//hпи использовании FromData не надо устанавливать заголовок, так как он устанавливается автоматически
+			//чтобы руками не пришлось формировать из форм объект, можно использовать класс FormData
+			//(нормально работает в php)
+			const formData = new FormData(form);
+			//в верстке, если данные идут на сервер, всегда надо указывать name в теге input
+
+			//если надо получить данные в формате JSON
+
+			//после open
+			//request.setRequestHeader('Content-type', 'application/son');
+			//const formData = new FormData(form); нельзя formdata заставить сделать JSON
+			//const object = {
+			//	formData.forEach(function(value, key) {
+				//object[key] = value;
+			//}) из списка от FromData мы создаем оъект перебором
+			//}
+			//const json = JSON.stringify(object); после чего помещаем в send вместо FormData
+			//php не умеет работать с json напрямую, в php надо дописать
+			//$_POST = json_decode(file_get_contents("php://input"), true);
+
+			request.send(formData);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					//console.log(request.response); //лог ответа от сервера
+					//который в PHP настроен как эхо нашего POST
+					//statusMassage.textContent = massage.//succes; //сообщение успеха
+					form.reset(); //очистка формы
+					//setTimeout(() => {
+					statusMassage.remove(); //удаление сообщения
+					//}, 2000);
+					showModalAfterSend(massage.succes); 
+				} else {
+					//statusMassage.textContent = massage.failure;
+					showModalAfterSend(massage.failure); 
+				}
+			});
+		});
+	}
+
+	function showModalAfterSend(text) {
+		if (!modal.classList.contains('modal__active')) {
+			toggleModal();
+		}
+
+		const prevModalDialog = document.querySelector('.modal__dialog');
+		prevModalDialog.classList.add('hide');
+
+		const massageModal = document.createElement('div');
+		massageModal.classList.add('modal__dialog');
+		massageModal.innerHTML = `<div class="modal__content">
+		<div data-close class="modal__close">&times;</div>
+		<div class="modal__title">${text}</div>
+		</div>`;
+		modal.append(massageModal);
+		
+		setTimeout(() => {
+			massageModal.remove();
+			prevModalDialog.classList.remove('hide');
+		}, 3000);
+	}
 });
